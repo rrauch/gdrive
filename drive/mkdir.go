fe0b211f2741c49c5275b55afa857975ad7c6473
Roland Rauch <roland@rauch.com>
2017-05-23 17:57:25 +0700

    added check for pre-existing directories in mkdir

--- drive/mkdir.go
+++ drive/mkdir.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"google.golang.org/api/drive/v3"
 	"io"
+        "strings"
 )
 
 const DirectoryMimeType = "application/vnd.google-apps.folder"
@@ -25,6 +26,23 @@ func (self *Drive) Mkdir(args MkdirArgs) error {
 }
 
 func (self *Drive) mkdir(args MkdirArgs) (*drive.File, error) {
+        // Checking for pre-existing directory
+        parentId := args.Parents[0]
+        name := strings.Replace(args.Name, "'", "\\'", -1)
+	query := fmt.Sprintf("trashed = false and mimeType = '%s' and name = '%s' and '%s' in parents", DirectoryMimeType, name, parentId)
+	result, err := self.service.Files.List().Q(query).Fields("files(id,name)").Do()
+	if err != nil {
+		return nil, err
+	}
+	if len(result.Files) == 0 {
+                // Directory does not exist yet, create it
+		return self.mkdirActual(args)
+	}
+	// Matching directory already exist, using it instead
+        return result.Files[0], nil
+}
+
+func (self *Drive) mkdirActual(args MkdirArgs) (*drive.File, error) {
 	dstFile := &drive.File{
 		Name:        args.Name,
 		Description: args.Description,
